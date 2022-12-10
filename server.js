63f85666d1cd3ebd7827101f1eec7a94a169a891
Minhazul Islam <minhazulislam@hotmail.com>
2022-12-10 20:32:34 +0000

    connecting notes.html to /notes endpoint

--- server.js
+++ server.js
@@ -14,6 +14,9 @@ app.get("/", (req,res) =>
     res.sendFile(path.join(__dirname, '/public/index.html'))
 );
 
+app.get("/notes",(req, res) =>
+    res.sendFile(path.join(__dirname, '/public/notes.html'))
+);
 
 
 app.listen(PORT, ()=>{
