4db5a9fdb5c77b92f02f811d9c2aa924374e0c24
Minhazul Islam <minhazulislam@hotmail.com>
2022-12-12 12:34:20 +0000

    modification to port, linked db.json object and added get method that renders db file

--- server.js
+++ server.js
@@ -1,21 +1,28 @@
 const express = require("express");
 const path = require('path');
 const app = express();
-const PORT = 3001;
+const PORT = process.env.PORT || 3001; //added today 
+const db = require("./db/db.json")
+
+
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 // connects the css files
 app.use(express.static('./public'));
 
-// it matters which order req and res are place for the code to execute
+
+// it dose matters which order req and res are placed for the code to execute
 app.get("/", (req,res) =>
     res.sendFile(path.join(__dirname, '/public/index.html'))
 );
 
 app.get("/notes",(req, res) =>
-    res.sendFile(path.join(__dirname, '/public/notes.html'))
+    res.sendFile(path.join(__dirname, '/public/notes.html'))    
 );
+
+app.get("/api/notes",(req, res) => res.json(db));
+
 
 app.listen(PORT, ()=>{
     console.log(`listening to port ${PORT}`);
